d782573db8bd5b2233d73d737643c929ce2cf700
Wilver Vargas <kryshot05@gmail.com>
2025-06-02 23:03:05 -0400

    proyecto modular creado

--- app-modular/src/app.module.ts
+++ app-modular/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuariosModule } from './modules/usuarios/usuarios.module';
+import { EspaciosModule } from './modules/espacios/espacios.module';
+import { ReservasModule } from './modules/reservas/reservas.module';
+import { NotificacionesModule } from './modules/notificaciones/notificaciones.module';
+import { DatabaseModule } from './common/database/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './common/configurations/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+    UsuariosModule,
+    EspaciosModule,
+    ReservasModule,
+    NotificacionesModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- app-modular/src/main.ts
+++ app-modular/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configSwagger, IPackageJson, printServerInitLog } from './common/utils/swagger.helper';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const packageJson = configService.get<IPackageJson>('packageJson')!;
+
+  configSwagger(app, packageJson);
+
+  const port = configService.get<number>('port') || 3001;
+  await app.listen(port, '0.0.0.0').then(async () => {
+    await printServerInitLog(app, packageJson);
+  });
 }
 bootstrap();
