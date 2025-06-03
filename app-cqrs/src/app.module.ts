bade2656f3a19fda72cb9d888c8ef7259859e34b
Wilver Vargas <kryshot05@gmail.com>
2025-06-03 00:07:15 -0400

    config class-validator

--- app-cqrs/src/app.module.ts
+++ app-cqrs/src/app.module.ts
@@ -3,12 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { ReservationsModule } from './modules/reservations/reservations.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
 import { PrismaService } from './shared/services/prisma.service';
+import configuration from './common/configurations/configuration';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
     ReservationsModule,
     NotificationsModule,
   ],
--- app-cqrs/src/main.ts
+++ app-cqrs/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { configSwagger, IPackageJson, printServerInitLog } from './common/utils/swagger.helper';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const packageJson = configService.get<IPackageJson>('packageJson')!;
+
+  configSwagger(app, packageJson);
+
+  const port = configService.get<number>('port') || 3001;
+  await app.listen(port, '0.0.0.0').then(async () => {
+    await printServerInitLog(app, packageJson);
+  });
 }
 bootstrap();
